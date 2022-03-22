1774b5108d6c5b0fce148d3b0cb091feffe59180
arthurariza <tucaariza@gmail.com>
2022-03-21 22:23:30 -0300

    refactor: sharing examples with a helper method and memoization

--- 01-getting-started/01/spec/sandwich_spec.rb
+++ 01-getting-started/01/spec/sandwich_spec.rb
@@ -1,23 +1,19 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
-  before do
-    @sandwich = Sandwich.new('delicious', [])
+  def sandwich
+    @sandwich ||= Sandwich.new('delicious', [])
   end
 
   it 'is delicious' do
-    sandwich = Sandwich.new('delicious', [])
-
-    taste = @sandwich.taste
+    taste = sandwich.taste
 
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add topping' do
-    sandwich = Sandwich.new('delicious', [])
-
-    @sandwich.toppings << 'cheese'
-    toppings = @sandwich.toppings
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
 
     expect(toppings).not_to be_empty
     expect(toppings).to include 'cheese'
